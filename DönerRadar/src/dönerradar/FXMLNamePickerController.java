c1e7a8cdc342d6cc446ce2825ecca8c82541e996
Jelle Widdershoven <jellewiddershoven@gmail.com>
2015-10-07 09:39:51 +0200

    Random name chooser aangepast

--- DönerRadar/src/dönerradar/FXMLNamePickerController.java
+++ DönerRadar/src/dönerradar/FXMLNamePickerController.java
@@ -64,6 +64,7 @@ public class FXMLNamePickerController implements Initializable {
     {
         if (!tfDeelnemerNaam.getText().isEmpty())
         {
+            tfDeelnemerNaam.clear();
             deelnemers.add(tfDeelnemerNaam.getText());
         }  
     }
