1ff5556648d088bff3af7ba9dbb7fe10bc77add9
JordyGuzak1991 <j.guzak@student.fontys.nl>
2015-10-07 09:40:55 +0200

    FXML Locatie kiezen en Restaurants

---
+++ DönerRadar/src/dönerradar/FXMLRestaurantsController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dönerradar;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author jordy
+ */
+public class FXMLRestaurantsController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
